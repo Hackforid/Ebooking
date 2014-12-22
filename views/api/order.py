bdd9291cec8ffd3cac4e51410fe23f5d129b1248
Sai <zhouquanbest@gmail.com>
2014-12-22 16:57:02 +0800

    add user confirm order api

--- views/api/order.py
+++ views/api/order.py
@@ -6,8 +6,10 @@ from tornado import gen
 from tools.auth import auth_login
 from views.base import BtwBaseHandler
 from exception.json_exception import JsonException
+from exception.celery_exception import CeleryException
 
-import tasks.models.order as Order
+from tasks.models import order as Order
+from tasks.order import submit_order as SubmitOrder
 
 class OrderWaitingAPIHandler(BtwBaseHandler):
 
@@ -27,3 +29,28 @@ class OrderWaitingAPIHandler(BtwBaseHandler):
         else:
             raise JsonException(errcode=1000, errmsg="wrong")
 
+
+class OrderOperateAPIHandler(BtwBaseHandler):
+
+    @gen.coroutine
+    @auth_login(json=True)
+    def post(self, order_id):
+        merchant_id = self.current_user.merchant_id
+
+        task = yield gen.Task(SubmitOrder.confirm_order_by_user.apply_async,
+                args=[merchant_id, order_id])
+        if task.status == 'SUCCESS':
+            self.finish_json(result=dict(
+                order=task.result.todict(),
+                ))
+        else:
+            if isinstance(task.result, CeleryException):
+                raise JsonException(1000, task.result.errmsg)
+            else:
+                raise JsonException(1000, 'network error')
+
+
+    @gen.coroutine
+    @auth_login(json=True)
+    def delete(self, order_id):
+        merchant_id = self.current_user.merchant_id
