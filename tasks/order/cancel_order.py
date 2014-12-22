e8dca453ecdc822f7d8fe3d3e7be066ccbca1ee1
Sai <zhouquanbest@gmail.com>
2014-12-22 18:43:49 +0800

    add server cancel order api

--- tasks/order/cancel_order.py
+++ tasks/order/cancel_order.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-]
 
-import datetime
-
 from tasks.celery_app import app
 from tasks.base_task import SqlAlchemyTask
 from tasks.order import cancel_order_in_queue as Cancel
@@ -10,24 +8,19 @@ from models.order import OrderModel
 from exception.celery_exception import CeleryException
 
 
-def cancel_before_user_confirm(session, order):
-    pass
-
-def cancel_after_user_confirm(session, order):
-    pass
-
-
 @app.task(base=SqlAlchemyTask, bind=True)
 def cancel_order_by_server(self, order_id):
     session = self.session
     order = OrderModel.get_by_id(session, order_id)
 
-    if order.status == 200:
-        pass
-    elif order.status == 0 or order.status == 100:
-        cancel_before_user_confirm(session, order)
+    if order.status == 0 or order.status == 100:
+        return cancel_before_user_confirm(session, order.id)
+    elif order.status == 200:
+        raise CeleryException(1000, 'illegal status')
     elif order.status == 300:
-        cancel_after_user_confirm(session, order)
+        return cancel_after_user_confirm(session, order.id)
+    else:
+        raise CeleryException(1000, 'illegal status')
 
 @app.task(base=SqlAlchemyTask, bind=True)
 def cancel_order_by_user(self, merchant_id, order_id):
@@ -47,3 +40,26 @@ def cancel_order_by_user(self, merchant_id, order_id):
     else:
         if isinstance(result, Exception):
             raise result
+
+
+def cancel_before_user_confirm(session, order_id):
+    task = Cancel.cancel_order_before_user_confirm.delay(order_id)
+    result = task.get()
+    
+    if task.status == 'SUCCESS':
+        return result
+    else:
+        if isinstance(result, Exception):
+            raise result
+
+def cancel_after_user_confirm(session, order_id):
+    task = Cancel.cancel_order_after_user_confirm.delay(order_id)
+    result = task.get()
+    
+    if task.status == 'SUCCESS':
+        return result
+    else:
+        if isinstance(result, Exception):
+            raise result
+
+
