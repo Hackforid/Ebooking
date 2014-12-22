74328fadda8e87bd6aa3418178cb417f199d184b
cjadmin <cjadmin@ubun-test.(none)>
2014-12-22 17:30:28 +0800

    temp for merge

---
+++ static/js/orderWaiting.js
@@ -0,0 +1,39 @@
+(function() {
+
+	var orderWaitingApp = angular.module('orderWaitingApp', []);
+	orderWaitingApp.controller('orderWaitingCtrl', ['$scope', '$http', function($scope, $http) {
+
+		/*var url=http://127.0.0.1:9501/api/order/waiting/
+		http://127.0.0.1:9501/order/waiting
+		http://127.0.0.1:9501/api/order/67/confirm
+
+		$http.get(url)
+				.success(function(resp) {
+					console.log(resp)
+					if (resp.errcode == 0) {
+						$scope.hotel = resp.result.hotel;
+						$scope.roomtypes = resp.result.cooped_roomtypes;
+						if ($scope.roomtypes.length > 0) {
+							$scope.currentRoomType = $scope.roomtypes[0];
+						}
+					} else {
+						alert(resp.errmsg);
+					}
+				})
+				.error(function() {
+					alert('network error');
+				})
+*/
+
+
+
+
+
+
+
+
+	}])
+
+
+
+	})()
