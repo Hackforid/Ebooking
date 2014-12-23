9dac4f6a6a57abb927b20cb1cdf1da34a8273f93
Sai <zhouquanbest@gmail.com>
2014-12-23 20:37:00 +0800

    hotel list api

--- tools/url.py
+++ tools/url.py
@@ -3,7 +3,7 @@
 def add_get_params(url, params):
     r = url
     query = '&'.join(['='.join((key, str(params.get(key)))) for key in params if params.get(key) is not None])
-    if query:
+    if query is not None:
         r = r + '?' + query
 
     return r
