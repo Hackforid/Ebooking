4fb0203b3e20ddedd96fe968b0893c485079038c
cjadmin <cjadmin@ubun-test.(none)>
2014-12-23 21:50:49 +0800

    modify hotelwillcoop

--- static/js/hotelWillCoop.js
+++ static/js/hotelWillCoop.js
@@ -8,6 +8,15 @@ hotelWillCoopApp.controller('hotelWillCoopContentCtrl',
 	$scope.citys = [];
 	$scope.hotels = [];
 
+
+	$scope.serchName="";
+	$scope.serchCity="";
+	$scope.serchStar="";
+	$scope.itemPerPage = 10;
+	$scope.currentPage;
+
+
+
 	function loadCitys() {
 		var url = "/api/city/";
 		$http.get(url)
@@ -37,6 +46,52 @@ hotelWillCoopApp.controller('hotelWillCoopContentCtrl',
 				alert('酒店列表读取失败');
 			});
 	}
+
+	$scope.searchHotel=function(){
+	console.log($scope.serchName);
+	console.log($scope.serchCity);
+	console.log($scope.serchStar);
+	console.log($scope.itemPerPage); //$scope.currentPage;
+	var url = '/api/hotel/willcoop/?start=0';
+
+	if($scope.serchName.trim()!="" && $scope.serchName!=undefined){
+		url=url+"&name="+$scope.serchName;
+
+	}
+	if($scope.serchCity.trim()!="" && $scope.serchCity!=undefined){
+
+		var cityId=getCityId($scope.serchCity);
+
+   		url=url+"&city_id="+cityId;
+ 	
+
+	}
+	if($scope.serchStar.trim()!="" && $scope.serchStar!=undefined){
+		url=url+"&star="+$scope.serchStar;
+
+	}
+
+	console.log(url);
+	//url='/api/hotel/willcoop/?star=1&city_id';
+
+	$http.get(url)
+			.success(function(resp) {
+				if (resp.errcode == 0) {
+					console.log(resp);
+					$scope.hotels = resp.result.hotels;
+				} else {
+					alert(resp.errmsg);
+				}
+
+			})
+			.error(function() {
+				alert('酒店列表读取失败');
+			});
+
+
+	}
+
+
 
 	$scope.cooprate = function(hotel) {
 
@@ -68,11 +123,23 @@ hotelWillCoopApp.controller('hotelWillCoopContentCtrl',
 
 		return '';
 	}
+
+	function getCityId (cityName) {
+	for (var i = 0; i < $scope.citys.length; i++) {
+			var city = $scope.citys[i];
+			if (city.name == cityName) {
+				return city.id;
+			}
+		}
+
+		return ' ';
+	}
+
 }])
-.controller('hotelWillCoopPageIndicatorCtrl',
+/*.controller('hotelWillCoopPageIndicatorCtrl',
 		['$scope', function($scope) {
 	$scope.itemPerPage = 10;
-}])
+}])*/
 
 
 
