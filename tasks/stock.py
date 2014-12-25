f8540832f565066832f7dc3459b9e2b397328033
Sai <zhouquanbest@gmail.com>
2014-12-25 14:44:28 +0800

    push hotel

--- tasks/stock.py
+++ tasks/stock.py
@@ -2,31 +2,56 @@
 
 import requests
 import json
+import time
 
 from tasks.celery_app import app
 from tasks.base_task import SqlAlchemyTask
 from tasks.order import cancel_order_in_queue as Cancel
 
 from models.order import OrderModel
+from models.cooperate_roomtype import CooperateRoomTypeModel
 from exception.celery_exception import CeleryException
 
 from config import CHAIN_ID, API
 
 @app.task(base=SqlAlchemyTask, bind=True, ignore_result=True)
-def push__hotel(hotel):
+def push_hotel(self, hotel_id):
+    from models.cooperate_hotel import CooperateHotelModel
 
-    base_hotel = fetch_base_hotel(hotel.base_hotel_id)
-    data = [generate_data(hotel, base_hotel)]
+    hotel = CooperateHotelModel.get_by_id(self.session, hotel_id)
+    if not hotel:
+        return
+
+    roomtypes = CooperateRoomTypeModel.get_by_hotel_id(self.session, hotel_id)
+
+    base_hotel, base_roomtypes = fetch_base_hotel_and_roomtypes(hotel.base_hotel_id)
+    if not base_hotel:
+        return
+
+    hotel_data = generate_data(hotel, roomtypes, base_hotel, base_roomtypes)
+    post_hotel(hotel_data)
+
+def post_hotel(hotel_data):
+    track_id = generate_track_id(hotel_data['id'])
+    data = {'list': [hotel_data]}
+    params = {'track_id': track_id, 'data': json.dumps(data)}
+    print params
+    url = API['STOCK'] + '/stock/update_hotel'
+    r = requests.post(url, data=params)
+    print r.text
+
+def generate_track_id(hotel_id):
+    return "{}|{}".format(hotel_id, time.time())
 
 def fetch_base_hotel(base_hotel_id):
     url = API['POI'] + "/api/hotel/" + str(base_hotel_id)
     r = requests.get(url)
     if r.status_code == 200:
         result = r.json()
-        if resutl['errorcode'] == 0:
+        if result['errorcode'] == 0:
             return result['result']['hotel']
 
-def generate_data(hotel, base_hotel):
+def generate_data(hotel, roomtypes, base_hotel, base_roomtypes):
     data = {}
     data['chain_id'] = CHAIN_ID
     data['id'] = hotel.id
@@ -37,9 +62,34 @@ def generate_data(hotel, base_hotel):
     data['address'] = base_hotel['address']
     data['star'] = base_hotel['star']
     data['tel'] = base_hotel['phone']
-    data['facilities'] = base_hotel['facilities']
-    data['room_types'] = []
+    data['facilities'] = base_hotel['facilities'].replace(',', '|') if base_hotel['facilities'] else ''
+    data['room_types'] = generate_rooms(roomtypes, base_roomtypes)
 
     return data
 
+def generate_rooms(roomtypes, base_roomtypes):
+    rooms = []
+    for roomtype in roomtypes:
+        for base_roomtype in base_roomtypes:
+            if base_roomtype['id'] == roomtype.base_roomtype_id:
+                room = {}
+                room['id'] = roomtype.id
+                room['name'] = base_roomtype['name']
+                room['bed_type'] = base_roomtype['bed_type']
+                room['facilities'] = base_roomtype['facilities'].replace(',', '|') if base_roomtype['facilities'] else ''
+                rooms.append(room)
+                break
+    return rooms
+
+
+
+def fetch_base_hotel_and_roomtypes(hotel_id):
+    url = API['POI'] + "/api/hotel/" + str(hotel_id) + "/roomtype/"
+    r = requests.get(url)
+    if r.status_code == 200:
+        result = r.json()
+        if result['errcode'] == 0:
+            return result['result']['hotel'], result['result']['roomtypes']
+
+    return None, None
 
