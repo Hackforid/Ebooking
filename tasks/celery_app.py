6aada39b4b5c8d228bd7a2af1a64e0d66c61f5f9
Sai <zhouquanbest@gmail.com>
2014-12-31 11:07:49 +0800

    add cortab

--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -5,16 +5,7 @@ from celery import Celery
 
 from config import Config
 
-app = Celery("celery_app",
-        broker="amqp://admin:admin@localhost:5672/ebooking",
-        backend ="amqp://admin:admin@localhost:5672/ebooking",
-        include=['tasks.test', 'tasks.stock',
-            'tasks.models.cooperate_hotel', 'tasks.models.cooperate_roomtype',
-            'tasks.models.inventory', 'tasks.models.rate_plan', 'tasks.models.room_rate',
-            'tasks.models.order', 'tasks.models.user',
-            'tasks.order.submit_order', 'tasks.order.submit_order_in_queue',
-            'tasks.order.cancel_order', 'tasks.order.cancel_order_in_queue'],
-        )
+app = Celery("celery_app")
 
 app.config_from_object('tasks.celery_config')
 
--- tasks/celery_config.py
+++ tasks/celery_config.py
@@ -1,13 +1,24 @@
 # -*- coding -*-
 
 
+from datetime import timedelta
 from celery.schedules import crontab
+
+BROKER_URL = "amqp://admin:admin@localhost:5672/ebooking"
+CELERY_RESULT_BACKEND = "amqp://admin:admin@localhost:5672/ebooking"
+CELERY_IMPORTS = (
+    'tasks.test', 'tasks.stock',
+                'tasks.models.cooperate_hotel', 'tasks.models.cooperate_roomtype',
+                'tasks.models.inventory', 'tasks.models.rate_plan', 'tasks.models.room_rate',
+                'tasks.models.order', 'tasks.models.user',
+                'tasks.order.submit_order', 'tasks.order.submit_order_in_queue',
+                'tasks.order.cancel_order', 'tasks.order.cancel_order_in_queue',)
 
 
 CELERYBEAT_SCHEDULE = {
         'complete_inventory_in_four_months': {
-            'task': 'tasks.models.inventory.test',
-            'schedule': 5.0,
-            'args': ('hello'),
+            'task': 'tasks.models.inventory.complete_in_four_months',
+            #'schedule': crontab(hour=9),
+            'schedule': timedelta(seconds=5),
             },
 }
