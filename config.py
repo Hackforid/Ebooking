3bdda6916f4a1fd561b540026b0548b006ae2b2e
Sai <zhouquanbest@gmail.com>
2015-01-07 12:33:59 +0800

    add beta config

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 Config = {
-    'db': 'mysql+mysqlconnector://root:root@192.168.10.15:3306/devine_ebooking?charset=utf8',
-    'mysql-connector': 'mysql+mysqlconnector://root:root@192.168.10.15:3306/devine_ebooking?charset=utf8',
+    'db': 'mysql+mysqlconnector://btw:btw@10.171.249.95:3306/devine_ebooking?charset=utf8',
+    'mysql-connector': 'mysql+mysqlconnector://btw:btw@10.171.249.95:3306/devine_ebooking?charset=utf8',
 }
 
 API = {
-    'POI': 'http://192.168.10.15:9000',
+    'POI': 'http://121.41.112.197:9001',
     'STOCK': 'http://121.41.23.128:8080/stock2',
     'ORDER': 'http://121.40.120.72:8080/',
 }
@@ -14,4 +14,4 @@ API = {
 
 CHAIN_ID = 6
 
-IS_PUSH_TO_STOCK = False
+IS_PUSH_TO_STOCK = True
