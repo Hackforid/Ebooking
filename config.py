366f72afd30d2425a392d7cb1369927baebdf671
Sai <zhouquanbest@gmail.com>
2015-01-08 17:41:22 +0800

    fix congfig

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 Config = {
-    'mysql-connector': 'mysql+mysqlconnector://btw:btw@10.171.249.95:3306/devine_ebooking?charset=utf8',
+    'mysql-connector': 'mysql+mysqlconnector://root:root@192.168.10.15:3306/devine_ebooking?charset=utf8',
 }
 
 API = {
-    'POI': 'http://121.41.112.197:9001',
+    'POI': 'http://192.168.10.15:9001',
     'STOCK': 'http://121.41.23.128:8080/stock2',
     'ORDER': 'http://121.40.120.72:8080/',
 }
@@ -13,7 +13,7 @@ API = {
 
 CHAIN_ID = 6
 
-IS_PUSH_TO_STOCK = True
+IS_PUSH_TO_STOCK = False
 
-BROKER_URL = "amqp://devine:devine@10.251.251.192:5672/ebooking"
-CELERY_RESULT_BACKEND = "amqp://:devine:devine@10.251.251.192:5672/ebooking"
+BROKER_URL = "amqp://admin:admin@localhost:5672/ebooking"
+CELERY_RESULT_BACKEND = "amqp://:admin:admin@localhost:5672/ebooking"
