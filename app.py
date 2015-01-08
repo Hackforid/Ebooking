af03aac91bd859d0e6d7becec403d5f5c99d56b0
Sai <zhouquanbest@gmail.com>
2015-01-08 19:42:43 +0800

    add listen ip config

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ class Application(tornado.web.Application):
 def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
-    http_server.listen(options.port)
+    http_server.listen(options.port, address=Config.LISTENER_IP)
     tornado.ioloop.IOLoop.instance().start()
 
 
