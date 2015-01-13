40a57173996fed2e08b0abb53aeb9e323777a5ab
Sai <zhouquanbest@gmail.com>
2015-01-13 18:41:02 +0800

    debug

--- tasks/db.py
+++ tasks/db.py
@@ -11,7 +11,7 @@ from config import Config
 engine = create_engine(
             Config['mysql-connector'], encoding='utf-8',
             pool_recycle=3600, pool_size=20,
-#            strategy='threadlocal'
+            echo=True
             )
 
 Session = scoped_session(sessionmaker(bind=engine, expire_on_commit=False))
