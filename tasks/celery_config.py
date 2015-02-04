957cd7b83d2eb5f3c4154e86ee8efce64c93d363
Sai <zhouquanbest@gmail.com>
2015-02-04 17:17:52 +0800

    rm roomrate task

--- tasks/celery_config.py
+++ tasks/celery_config.py
@@ -13,7 +13,7 @@ CELERYD_PREFETCH_MULTIPLIER = 2
 CELERY_IMPORTS = (
     'tasks.test', 'tasks.stock', 'tasks.poi',
                 'tasks.models.cooperate_hotel', 'tasks.models.cooperate_roomtype',
-                'tasks.models.inventory', 'tasks.models.room_rate',
+                'tasks.models.inventory',
                 'tasks.models.order', 'tasks.models.merchant',
                 'tasks.order.submit_order', 'tasks.order.submit_order_in_queue',
                 'tasks.order.cancel_order', 'tasks.order.cancel_order_in_queue',)
