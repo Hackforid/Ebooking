438f50d352d771d2ca5feefc2c5378756e85c7f7
cjadmin <cjadmin@ubun-test.(none)>
2015-02-05 10:55:13 +0800

    modify username

--- static/java/js/order.js
+++ static/java/js/order.js
@@ -663,7 +663,46 @@ orderAnalyseApp.controller('orderTab1AnalyseCtrl', ['$scope', '$http', function(
 
                     }
 
-                    $("#usenameId").html(resp.result.username);
+
+                    var nameCheckValue = $.trim(resp.result.username);
+                    var nameLength = nameCheckValue.replace(/[\u4e00-\u9fa5]/g, "aa").length;
+                    if (nameLength > 6) {
+
+                        var cordCount = 0;
+                        var realName = "";
+                        for (var i = 0; i < 6; i++) {
+                            var singleCode = nameCheckValue.substring(i, i + 1);
+
+                            if (/[\u4e00-\u9fa5]/.test(singleCode)) {
+
+                                cordCount = cordCount + 2;
+                                realName = realName + singleCode;
+                                if (cordCount > 4) {
+                                    break;
+                                }
+
+                            } else {
+                                cordCount = cordCount + 1;
+                                realName = realName + singleCode;
+                                if (cordCount > 6) {
+                                    break;
+                                }
+
+                            }
+
+                        };
+                        realName = realName + "...";
+
+                        //console.log(realName);
+                        $("#usenameId").html(realName);
+
+                    } else {
+
+                        $("#usenameId").html(resp.result.username);
+
+                    }
+
+
                     $scope.chartInit.init();
 
 
