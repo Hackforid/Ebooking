7fa177becc69d0bafbac5cc0a022400ed971de54
Sai <zhouquanbest@gmail.com>
2015-02-11 18:47:23 +0800

    update

--- views/api/finance.py
+++ views/api/finance.py
@@ -2,11 +2,6 @@
 
 from datetime import date
 
-from tornado import gen
-from tornado.httpclient import AsyncHTTPClient
-from tornado.escape import json_encode, json_decode
-
-from config import API
 from views.base import BtwBaseHandler
 from models.order import OrderModel
 from models.income import IncomeModel
@@ -68,25 +63,24 @@ class IncomeAPIHandler(BtwBaseHandler):
     def post(self):
         merchant_id =self.current_user.merchant_id
         args = self.get_json_arguments()
-        pay_type, ota_id, year, month, value = get_and_valid_arguments(
-                args, 'pay_type', 'ota_id', 'year', 'month', 'value'
+        pay_type, ota_ids, year, month, value = get_and_valid_arguments(
+                args, 'pay_type', 'ota_ids', 'year', 'month', 'value'
                 )
         remark = args.get('remark', '')
-        pay_type, ota_id, year, month, value = int(pay_type), int(ota_id), int(year), int(month), int(value)
-        self.valid_args(pay_type, ota_id, year, month, value, remark)
+        self.valid_args(pay_type, ota_ids, year, month, value, remark)
 
-        income = IncomeModel.new(self.db, merchant_id, pay_type, ota_id, year, month, value, remark)
+        income = IncomeModel.new(self.db, merchant_id, pay_type, min(ota_ids), year, month, value, remark)
 
         self.finish_json(result=dict(
             income=income.todict(),
             ))
 
-    def valid_args(self, pay_type, ota_id, year, month, value, remark):
+    def valid_args(self, pay_type, ota_ids, year, month, value, remark):
         if pay_type not in [0, 1]:
             raise JsonException(2001, 'invalid pay_type')
 
-        if ota_id not in OTA:
-            raise JsonException(2002, 'invalid ota_id')
+        #if ota_id not in OTA:
+            #raise JsonException(2002, 'invalid ota_id')
 
         if year <= 2000:
             raise JsonException(2003, 'invalid year')
