73cefb1c7e41e1eb1f7f4b2351c3ace122ef0e3b
Sai <zhouquanbest@gmail.com>
2015-07-10 10:47:27 +0800

    change api

--- utils/ota.py
+++ utils/ota.py
@@ -36,7 +36,10 @@ def change_ota(hotel_id, ota_ids):
     Log.info("change ota>>hotel {}>>ota {}".format(hotel_id, ota_ids))
     url =  API['STOCK'] + '/stock/switch_ota'
     track_id = generate_track_id(hotel_id)
-    ota_ids_str = '|'.join([str(id) for id in ota_ids])
+    if not ota_ids:
+        ota_ids_str = '-1'
+    else:
+        ota_ids_str = '|'.join([str(id) for id in ota_ids])
     data = {
             'track_id': track_id,
             'chain_id': CHAIN_ID,
